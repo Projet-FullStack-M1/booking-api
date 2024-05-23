16d58326dc037561804f10e4737246af44b769d3
MalicknND <ndiayemalick062@gmail.com>
2024-05-23 15:44:37 +0200

    feat: Update verifyToken middleware to use authorization header

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 exports.verifyToken = (req, res, next) => {
-  // const token = req.headers.authorization?.split(" ")[1];
-  const token = req.cookies.token;
+  const token = req.headers.authorization?.split(" ")[1];
+  // const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({
