a5c5042f8f9e12c9828c505f604e42ce2466f154
MalicknND <ndiayemalick062@gmail.com>
2024-06-03 23:02:50 +0200

    🚩 feat: Ajouter la fonctionnalité de sauvegarde des publications

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const prisma = require("../../prisma/prisma");
 const bcrypt = require("bcrypt");
+const { ObjectId } = require("bson");
 
 exports.getUsers = async (req, res) => {
   try {
@@ -73,6 +74,41 @@ exports.deleteUser = async (req, res) => {
       where: { id },
     });
     res.status(200).json({ message: "User deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to delete users!" });
+  }
+};
+exports.savePost = async (req, res) => {
+  const postId = req.body.postId;
+  const tokenUserId = req.userId;
+
+  try {
+    const savedPost = await prisma.savedPost.findUnique({
+      where: {
+        userId_postId: {
+          userId: tokenUserId,
+          postId,
+        },
+      },
+    });
+
+    if (savedPost) {
+      await prisma.savedPost.delete({
+        where: {
+          id: savedPost.id,
+        },
+      });
+      res.status(200).json({ message: "Post removed from saved list" });
+    } else {
+      await prisma.savedPost.create({
+        data: {
+          userId: tokenUserId,
+          postId,
+        },
+      });
+      res.status(200).json({ message: "Post saved" });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to delete users!" });
